94a7d28c026e00d36cc52f32f27b947d5e7a923b
Rhea Vichot <33607544+vichotr@users.noreply.github.com>
2017-11-27 11:34:18 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+var sketch = function( p ) { 
+// Learning Processing
+// Daniel Shiffman
+// http://www.learningprocessing.com
+
+// Example 1-3: RGB Color
+p.background(255) {
+p.noStroke() {
+
+// Bright red
+p.fill(255, 0, 0) {
+p.ellipse(20, 20, 16, 16) {
+
+// Dark red
+p.fill(127, 0, 0) {
+p.ellipse(40, 20, 16, 16) {
+
+// Pink (pale red)
+p.fill(255, 200, 200) {
+p.ellipse(60, 20, 16, 16) { 
+ };
+
+var sketch = new p5(sketch);
